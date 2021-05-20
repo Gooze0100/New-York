f3097fc8fa24369a17d84943427dd36de54bba4b
Gooze0100 <kalniusg@gmail.com>
2021-05-20 22:16:58 +0300

    Fixed arrows for slideshow. Change position for search bar. Made all page with media queries.

--- index.js
+++ index.js
@@ -33,10 +33,39 @@
 // const slideIndex = 1;
 // showSlides(slideIndex);
 
-// funtion showSlides(n) {
+// function showSlides(n) {
 
 // }
 
+const bgImg1 = document.querySelector('#bgImg1')
+const bgImg2 = document.querySelector('#bgImg2')
+const bgImg3 = document.querySelector('#bgImg3')
+// const leftArr = document.querySelector('#leftArr').addEventListener('click', currentSlide(-1));
+// const rightArr = document.querySelector('#rightArr').addEventListener('click', currentSlide(1));
+// const dot1 = document.querySelector('#dot1');
+// const dot2 = document.querySelector('#dot2');
+// const dot3 = document.querySelector('#dot3');
+
+// const slideIndex = 1;
+// changeImg(slideIndex);
+
+// function plusSlides(n) {
+//     changeImg(slideIndex += n);
+// }
+  
+// function currentSlide(n) {
+//     changeImg(slideIndex = n);
+// }
+
+// function changeImg(n) {
+//     const slides = document.querySelector("#bgImg");
+//     if (n > slides.length) {slideIndex = 1}    
+//     if (n < 1) {slideIndex = slides.length}
+//     for (let i = 0; i < slides.length; i++) {
+//         slides[i].style.zIndex = "-2";  
+//     }
+//     slides[slideIndex-1].style.zIndex = "2";
+// }
 
 // Counter
 const quantity = document.querySelector('#quantity');
@@ -44,7 +73,7 @@ document.querySelector('#minus').addEventListener('click', extract);
 document.querySelector('#plus').addEventListener('click', add);
 
 function extract() {
-    if(quantity.value > 1) {
+    if (quantity.value > 1) {
         quantity.value--
     }
 }
